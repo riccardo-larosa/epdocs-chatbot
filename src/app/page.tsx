32544d46a3240a390ceb3f1fbff3ecf460080add
Riccardo La Rosa <r.larosa@gmail.com>
2024-11-20 15:54:11 -0500

    added redirect on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-<div>
-
-      <div className='overview-text'>
-
-        <h1 style={{ fontWeight: 'bold', fontSize: '2em' }}>Chatbot for EP Docs</h1>
-        <br />
-        
-        
-      </div>
-    </div>
-  );
+  redirect('/ask');
 }
