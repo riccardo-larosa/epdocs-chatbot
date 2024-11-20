e933b7dfa1caef349087deaec261dd0adc950590
Riccardo La Rosa <r.larosa@gmail.com>
2024-11-20 17:29:20 -0500

    fixed instrumentation logging

--- src/instrumentation.js
+++ src/instrumentation.js
@@ -3,7 +3,7 @@ import { AISDKExporter } from "langsmith/vercel";
 
 export function register() {
   console.log("registering instrumentation");
-  console.log(`process.env.LANGSMITH_API_KEY: ${process.env.LANGSMITH_API_KEY?.slice(0, 5)}...`);
+  console.log(`process.env.LANGCHAIN_API_KEY: ${process.env.LANGCHAIN_API_KEY?.slice(0, 5)}...`);
   registerOTel({
     serviceName: "langsmith-vercel-ai-sdk",
     traceExporter: new AISDKExporter(),
